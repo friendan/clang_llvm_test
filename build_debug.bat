d673979944730aebb73a2becd270f4ccbd79fda4
friendan <407583729@qq.com>
2026-05-25 01:15:48 +0800

    init

--- build_debug.bat
+++ build_debug.bat
@@ -1,8 +1,5 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-set LLVM_ROOT=D:\APP\LLVM\clang+llvm-18.1.8-x86_64-pc-windows-msvc
-set PATH=%LLVM_ROOT%\bin;D:\APP\LLVM;%PATH%
+call init_env.bat
 
 cmake --build build --config Debug
 @cmd /k
--- build_sln.bat
+++ build_sln.bat
@@ -1,8 +1,5 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-set LLVM_ROOT=D:\APP\LLVM\clang+llvm-18.1.8-x86_64-pc-windows-msvc
-set PATH=%LLVM_ROOT%\bin;D:\APP\LLVM;%PATH%
+call init_env.bat
 
 rmdir /s /q build
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
