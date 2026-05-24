e507381ce69b613f4e90b6a1992bfcaa03976173
friendan <407583729@qq.com>
2026-05-25 02:21:42 +0800

    init

--- build_vs_sln.bat
+++ build_vs_sln.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set LLVM_ROOT=D:\APP\LLVM\clang+llvm-18.1.8-x86_64-pc-windows-msvc
 set PATH=%LLVM_ROOT%\bin;D:\APP\LLVM;%PATH%
