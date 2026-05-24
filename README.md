b359df57abc6f1a0e4af594f841b0c93de1266c1
friendan <407583729@qq.com>
2026-05-25 00:42:01 +0800

    init

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ clang_llvm
 Ninja
 NMake
 C++ Clang tools for Windows
+
+如果你装了 "C++ Clang tools for Windows" 组件，就可以直接在 VS 工程属性里选择 `LLVM (clang-cl)` 平台工具集，
+然后用 VS 直接编译。独立 LLVM 包不提供这个 VS 集成。
+
+目前你用的是 NMake + clang-cl + lld-link 的路线，不需要这个 VS 组件也能正常用 LLVM 编译。
+如果你想回到 VS 生成器 + .sln 的路线，就需要安装那个组件。
--- build_sln.bat
+++ build_sln.bat
@@ -2,9 +2,9 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set LLVM_ROOT=D:\APP\LLVM\clang+llvm-18.1.8-x86_64-pc-windows-msvc
-set PATH=%LLVM_ROOT%\bin;%PATH%
+set PATH=%LLVM_ROOT%\bin;D:\APP\LLVM;%PATH%
 
 rmdir /s /q build
-cmake -B build -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 pause
